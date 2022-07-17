a9a0be14bb423008f7a4a07779d108564b840e70
jeffrey2423 <riosjeff24@gmail.com>
2022-07-16 19:55:09 -0500

    Inicio punto 2

--- src/controllers/CopiaLibrosController.ts
+++ src/controllers/CopiaLibrosController.ts
@@ -1,21 +1,31 @@
 import Utils from "../utils/Utils";
 import { ILibro } from "../interfaces/ILibro";
+import { join } from "path/posix";
 
-class CopiaLibrosController{
-    private problemData: string;
-    private problemDataArray: string[];
-    private libros: ILibro[];
-    private cantidadEscritores: number;
-    private cantidadLibros: number;
-  
-    constructor() {
-      this.problemData = "";
-      this.problemDataArray = [];
-      this.libros = [];
-      this.cantidadEscritores = 0;
-      this.cantidadLibros = 0;
-    }
-    
+
+
+class CopiaLibrosController {
+  private problemData: string;
+  private problemDataArray: string[];
+  private libros: ILibro[];
+  private cantidadEscritores: number;
+  private cantidadLibros: number;
+  private solutionFileData: string;
+
+  constructor() {
+    this.problemData = "";
+    this.problemDataArray = [];
+    this.libros = [];
+    this.cantidadEscritores = 0;
+    this.cantidadLibros = 0;
+    this.solutionFileData = "";
+  }
+
+  getSolutionFileData(): string {
+    return this.solutionFileData;
+  }
+
+  //*********************************SE INICIALIZA LA INFORMACION DEL PROBLEMA**************************/
   public InitializeProblemData(fileData: any): void {
     try {
       this.problemData = Utils.ReadFileData(fileData);
@@ -76,6 +86,100 @@ class CopiaLibrosController{
       throw error;
     }
   }
+  //*********************************SE INICIALIZA LA INFORMACION DEL PROBLEMA**************************/
+
+  //*********************************CODIGO SOLUCION RECURSIVA******************************************/
+
+  /**
+   * ***********************************************************************************************
+   * *****************************************ALGORITMO*********************************************
+   * ***********************************************************************************************
+   * Ordena los números en orden descendente
+   * Luego, suma iterativamente el siguiente número de paginas más grande
+   * a un conjunto en el que la suma actual sea la más pequeña, obteniendo ese conjunto
+   * de ordenar ascendentemente el arreglo de subsets en su propiedad sum
+   * 
+   * ***********************************************************************************************
+   * **********************************COMPLEJIDAD DEL TIEMPO***************************************
+   * ***********************************************************************************************
+   * 1.Estamos ordenando el erreglo, eso nos toma un tiempo de O(n log n) en el peor de los casos.
+   * 2.Para se esta iterando sobre el arreglo, eso nos toma un tiempo de O(n) en el peor de los casos.
+   * 3.En cada iteracion ordenamos el arreglo de subsets, eso nos toma un tiempo de O(n log n) en el peor de los casos.
+   * 
+   * En total, eso nos toma un tiempo de O(n log n) + O(n(n log n)).
+   * 
+   * Si contamos el tiempo que nos toma recorrer el arreglo solucion debemos agregar un tiempo de O(n^2), ya que debemos recorrer
+   * un arrego de arreglos (los subsets obtenidos)
+   * Siendo; O(n log n) + O(n(n log n)) + O(n^2)
+   */
+  private greedyPartition(libros: ILibro[], escritores: number): ILibro[] {
+    try {
+
+      libros.sort((a, b) => b.paginas - a.paginas);
+
+      const out: any[] = [...Array(escritores)].map((x) => {
+        return {
+          sum: 0,
+          libros: [],
+        };
+      });
+  
+      for (const elem of libros) {
+        const chosenSubset = out.sort((a, b) => a.sum - b.sum)[0];
+        chosenSubset.libros.push(elem);
+        chosenSubset.sum += elem.paginas;
+      }
+
+      return out.map(subset => subset.libros);
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private GenerarSolucionGreedyPartition(arr: any[]): void {
+    try {
+      let numeroDias: number = 0;
+      let totalPorEscritor: number = 0;    
+
+      for (let i = 0; i < arr.length; i++) {
+        totalPorEscritor = 0;
+        this.solutionFileData += `Escritor ${i + 1}:\n`;
+        for (let j = 0; j < arr[i].length; j++) {
+          numeroDias += arr[i][j].paginas;
+          this.solutionFileData += `  ${arr[i][j].nombre} ${arr[i][j].paginas}\n`;
+          totalPorEscritor += arr[i][j].paginas;
+        }
+        this.solutionFileData += `Total: ${totalPorEscritor}\n`;
+      }
+      this.solutionFileData = `${numeroDias}\n` + this.solutionFileData
+
+    } catch (error) {
+      throw error;
+    }
+  }
+    
+
+  public LlamarSolucionGreedyPartition(): void {
+    try {
+     let prueba: any[] = this.greedyPartition(this.libros, this.cantidadEscritores);
+     this.GenerarSolucionGreedyPartition(prueba);
+    } catch (error) {
+      throw error;
+    }
+  }
+  //*********************************CODIGO SOLUCION RECURSIVA******************************************/
+
+
+  //*********************************CODIGO SOLUCION DINAMICA******************************************/
+  public LlamarSolucionDinamica(): void {
+    try {
+
+    } catch (error) {
+      throw error;
+    }
+  }
+  //*********************************CODIGO SOLUCION DINAMICA******************************************/
 }
 
 export default CopiaLibrosController;
--- src/services/CopiaLibrosServices.ts
+++ src/services/CopiaLibrosServices.ts
@@ -4,7 +4,7 @@ import Utils from "../utils/Utils";
 import CopiaLibrosController from "../controllers/CopiaLibrosController";
 
 class CopiaLibrosServices {
-  private copiaLibrosController: CopiaLibrosController;
+  private copiaLibrosController!: CopiaLibrosController;
 
   constructor() {
    this.copiaLibrosController = new CopiaLibrosController();
@@ -13,17 +13,18 @@ class CopiaLibrosServices {
   public SolucionRecursiva(req: any, res: Response): void {
     try {
 
+      this.copiaLibrosController = new CopiaLibrosController();
+
       if (Utils.IsUndefinedOrNullOrEmptyOrFalse(req.files?.file)) {
         res.status(400).send("No se ha enviado ningun archivo");
         return;
       }
-
       this.copiaLibrosController.InitializeProblemData(req.files?.file.data);
-
+      this.copiaLibrosController.LlamarSolucionGreedyPartition();
       Utils.SendSolutionFile(
         req,
         res,
-        "",
+        this.copiaLibrosController.getSolutionFileData(),
         "RecursivaCopiaLibros"
       );
     } catch (error: any) {
@@ -33,16 +34,20 @@ class CopiaLibrosServices {
 
   public SolucionDinamica(req: any, res: Response): void {
     try {
+
+      this.copiaLibrosController = new CopiaLibrosController();
+
       if (Utils.IsUndefinedOrNullOrEmptyOrFalse(req.files?.file)) {
         res.status(Utils.HTTPStatus.INTERNAL_SERVER_ERROR).send("No se ha enviado ningun archivo");
         return;
       }
       this.copiaLibrosController.InitializeProblemData(req.files?.file.data);
+      this.copiaLibrosController.LlamarSolucionDinamica();
       Utils.SendSolutionFile(
         req,
         res,
-       '',
-       "DinamicaCopiaLibros"
+        this.copiaLibrosController.getSolutionFileData(),
+        "DinamicaCopiaLibros"
       );
     } catch (error: any) {
       res.status(Utils.HTTPStatus.INTERNAL_SERVER_ERROR).send(error.message);
